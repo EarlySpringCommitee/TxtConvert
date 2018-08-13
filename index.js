0890f248ed29c6e15077d02327a2023df950c776
Rex Tseng <rexx0520@gmail.com>
2018-08-14 03:48:00 +0800

    支援大型檔案轉換 支援編碼選擇

--- index.js
+++ index.js
@@ -1,40 +1,71 @@
+var varray;
+
 window.onload = () => {
     document.querySelector('#upload').addEventListener('change', handleFile, false)
+    document.getElementById('encode').onchange = function() {
+        let encode = this.options[this.selectedIndex].value;
+        $('#preview pre').text((new TextDecoder(encode)).decode(varray.slice(0,100)))
+    }
 
     function handleFile(e) {
-        var reader = new FileReader;
-        var file = e.target.files[0];
-        var fileName = e.target.files[0].name
+        let reader = new FileReader;
+        let file = e.target.files[0];
+        let fileName = e.target.files[0].name
         $('#uploader span.description').text(fileName)
         $('#download a').attr('download', fileName).html(`<i class='download icon'></i>下載 ${fileName}`)
         reader.onload = async(e) => {
+            let buffer = e.target.result
+            varray = new Uint8Array(buffer)
             $('#download,#error').attr('style', 'display:none')
                 //預覽
-            $('#preview pre').text(e.target.result)
+            $('#preview pre').text((new TextDecoder(getEncode())).decode(varray.slice(0,100)))
             $('#preview').removeAttr('style')
                 //轉換囉
             $('[data-start]').click(function() {
                 $('#preview').attr('style', 'display:none')
                 $('#loader').addClass("active indeterminate")
-                convent(e.target.result)
+                let text = (new TextDecoder(getEncode())).decode(varray)
+                convert(text)
             })
         }
-        reader.readAsText(file);
+        reader.readAsArrayBuffer(file);
     }
 }
-async function convent(t) {
+
+function getEncode() {
+    let node = document.getElementById('encode');
+    return node.options[node.selectedIndex].value;
+}
+
+function getByteLength(s) {
+    return (new TextEncoder()).encode(s).length
+}
+
+async function convert(t) {
     try {
-        var req = await axios({
+        let maxTextLength = parseInt(((await axios.get('https://api.zhconvert.org/service-info')).data.data.maxPostBodyBytes)/4)
+        let i = 0, arr = []
+        while(i < t.length) {
+            let index = t.lastIndexOf('\n', i+maxTextLength)+1
+            if (i == index) {
+                arr.push(t.substring(i, Infinity))
+                break;
+            } else {
+                arr.push(t.substring(i, index))
+                i = index
+            }
+        }
+        let result = (await Promise.all(arr.map(async(x) => (await axios({
             method: 'post',
             url: 'https://api.zhconvert.org/convert',
             data: {
                 converter: 'Taiwan',
-                text: t
+                text: x
             }
-        });
+        })).data.data.text))).join('')
 
-        var data = new Blob([req.data.data.text], { type: 'text/plain;charset=utf-8;' });
-        var url = URL.createObjectURL(data);
+        let data = new Blob([result], { type: 'text/plain;charset=utf-8;' });
+        let url = URL.createObjectURL(data);
 
         $('#download').removeAttr('style')
         $('#download a').attr('href', url)
